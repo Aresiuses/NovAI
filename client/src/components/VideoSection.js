3a36134049c487efd35864fdafe6a54510d74078
Aresiuses <adelrahman69@gmail.com>
2026-01-07 19:59:27 +0800

    Update VideoSection.js

--- client/src/components/VideoSection.js
+++ client/src/components/VideoSection.js
@@ -45,12 +45,12 @@ const VideoSection = () => {
               />
             )}
           </div>
-          <p className="video-description">
+        </div>
+      </div>
+      <p className="video-description">
             Watch our company introduction video to learn more about our journey 
             as CS graduates and the AI solutions we build.
           </p>
-        </div>
-      </div>
     </section>
   );
 };
